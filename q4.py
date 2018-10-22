935a12c95b94f7babde0ac49e78e814dc8fcefc2
aatack <alexanderjtatack@gmail.com>
2018-10-22 01:07:41 +0100

    Add debug code to work out input type of working machines in q4

--- q4.py
+++ q4.py
@@ -5,6 +5,11 @@ import numpy as np
 #compute all combinations for two portfolios
 def question04(rows, numberMachines):
   # modify and then return the variable below
+  for value in rows[0]:
+    if value is None:
+      raise Exception('X = None')
+    elif value == 0:
+      raise Exception('X = 0')
   answer = -1
   return answer
 
